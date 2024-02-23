16fbcef96309039ce48fa94507b33430ad805594
thinh <anvanthinh@gmail.com>
2024-02-23 23:21:46 +0700

    init

---
+++ src/main/java/com/thinh/flashcardlearningservice/FlashCardLearningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thinh.flashcardlearningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardLearningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardLearningServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinh/flashcardlearningservice/FlashCardLearningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinh.flashcardlearningservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardLearningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
