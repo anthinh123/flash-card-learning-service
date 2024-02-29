6511896c8215583b8d53269bcd0e8ada2e8ee2a8
thinh <anvanthinh@gmail.com>
2024-03-01 00:33:45 +0700

    config to run server configuration

--- src/main/java/com/thinh/flashcardlearningservice/FlashCardLearningServiceApplication.java
+++ src/main/java/com/thinh/flashcardlearningservice/FlashCardLearningServiceApplication.java
@@ -2,12 +2,16 @@ package com.thinh.flashcardlearningservice;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Configuration
+@EnableAutoConfiguration
 public class FlashCardLearningServiceApplication {
 	@Bean
 	public ModelMapper modelMapper(){
